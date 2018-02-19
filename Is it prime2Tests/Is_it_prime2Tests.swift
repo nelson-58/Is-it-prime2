d97ec85402e381e80f0712a42d267e2a1f69d603
Andy Harris <andy.harris@live.co.uk>
2018-02-19 10:50:53 +0000

    Initial Commit

---
+++ Is it prime2Tests/Is_it_prime2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Is_it_prime2Tests.swift
+//  Is it prime2Tests
+//
+//  Created by Andy Harris on 19/02/2018.
+//  Copyright © 2018 Andy Harris. All rights reserved.
+//
+
+import XCTest
+@testable import Is_it_prime2
+
+class Is_it_prime2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
